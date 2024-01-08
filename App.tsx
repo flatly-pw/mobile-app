8a00d907b6b911a9b6d8a8890f0d48ffaffc14fc
kemeyr <k.kaminski728@gmail.com>
2024-01-08 21:11:41 +0100

    feat: add hello world

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+import HelloWorldFeature from "./features/HelloWorldFeature/HelloWorldFeature";
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <HelloWorldFeature />
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/HelloWorld/HelloWorld.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function HelloWorld() {
+  return (
+    <View>
+      <Text>Hello world</Text>
+    </View>
+  );
+}
