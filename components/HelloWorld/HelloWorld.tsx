2c690844f42179b38e7e43d6e76e3669336ab8a3
kemeyr <k.kaminski728@gmail.com>
2024-01-08 21:16:17 +0100

    fix: change message in HelloWorld.tsx

--- components/HelloWorld/HelloWorld.tsx
+++ components/HelloWorld/HelloWorld.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function HelloWorld() {
   return (
     <View>
-      <Text>Hello world</Text>
+      <Text>Hello world!</Text>
     </View>
   );
 }
