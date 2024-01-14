cf5e858f0b4f14eb4569769bae37b3418060afcc
zeraye <zeraye@protonmail.com>
2024-01-14 16:32:57 +0100

    feat: add surface component to flat offer list item

--- features/HomeTab/FlatOfferList/FlatOfferListItem/FlatOfferListItem.tsx
+++ features/HomeTab/FlatOfferList/FlatOfferListItem/FlatOfferListItem.tsx
@@ -1,5 +1,6 @@
 import { Octicons } from "@expo/vector-icons";
 import { Text, View, Image, Pressable, Animated } from "react-native";
+import { Surface } from "react-native-paper";
 
 import FlatOffer from "../../../../interfaces/FlatOffer";
 
@@ -38,63 +39,69 @@ const FlatOfferListItem = ({ route, navigation, flatOffer }: FlatOfferListItemPr
         style={{
           padding: 15,
         }}>
-        <Animated.View style={{ opacity: animated }}>
-          <Image
-            style={{
-              height: 300,
-              borderRadius: 10,
-            }}
-            source={{
-              uri: flatOffer.imageSource,
-            }}
-          />
+        <Surface
+          style={{
+            borderRadius: 10,
+            padding: 5,
+          }}>
+          <Animated.View style={{ opacity: animated }}>
+            <Image
+              style={{
+                height: 300,
+                borderRadius: 10,
+              }}
+              source={{
+                uri: flatOffer.imageSource,
+              }}
+            />
 
-          <View
-            style={{
-              padding: 5,
-            }}>
             <View
               style={{
-                flexDirection: "row",
-                justifyContent: "space-between",
-                alignItems: "center",
+                padding: 5,
               }}>
-              <Text
-                style={{
-                  fontSize: 28,
-                  fontWeight: "bold",
-                }}>
-                {flatOffer.name}, {flatOffer.city}
-              </Text>
               <View
                 style={{
                   flexDirection: "row",
+                  justifyContent: "space-between",
                   alignItems: "center",
                 }}>
-                <Octicons name="star-fill" size={24} color="black" />
                 <Text
                   style={{
-                    padding: 5,
+                    fontSize: 28,
+                    fontWeight: "bold",
                   }}>
-                  {flatOffer.rating.toFixed(2)}
+                  {flatOffer.name}, {flatOffer.city}
                 </Text>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                  }}>
+                  <Octicons name="star-fill" size={24} color="black" />
+                  <Text
+                    style={{
+                      padding: 5,
+                    }}>
+                    {flatOffer.rating.toFixed(2)}
+                  </Text>
+                </View>
               </View>
-            </View>
 
-            <Text
-              style={{
-                fontSize: 12,
-              }}>
-              {flatOffer.distanceFromCenter.toFixed(0)} km from center
-            </Text>
-            <Text
-              style={{
-                fontSize: 20,
-              }}>
-              ${flatOffer.price.toFixed(0)} night
-            </Text>
-          </View>
-        </Animated.View>
+              <Text
+                style={{
+                  fontSize: 12,
+                }}>
+                {flatOffer.distanceFromCenter.toFixed(0)} km from center
+              </Text>
+              <Text
+                style={{
+                  fontSize: 20,
+                }}>
+                ${flatOffer.price.toFixed(0)} night
+              </Text>
+            </View>
+          </Animated.View>
+        </Surface>
       </View>
     </Pressable>
   );
