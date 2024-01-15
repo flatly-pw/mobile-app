81de6532a38db17eb2bd31a988353af70dcb6f4d
zeraye <zeraye@protonmail.com>
2024-01-15 23:03:50 +0100

    feat: update SignInScreen appearance

--- features/AuthScreen/SignInScreen/SignInScreen.tsx
+++ features/AuthScreen/SignInScreen/SignInScreen.tsx
@@ -1,34 +1,116 @@
-import React from "react";
-import { View } from "react-native";
-import { Button, Text, TextInput } from "react-native-paper";
+import { useContext, useState } from "react";
+import { Pressable, View } from "react-native";
+import { Button, HelperText, Text, useTheme } from "react-native-paper";
 
 import SafeAreaScreenWrapper from "../../../components/SafeAreaScreenWrapper/SafeAreaScreenWrapper";
 import AuthContext from "../../../contexts/AuthContext";
 import { ScreenType } from "../AuthScreen";
+import CustomTextInput from "../CustomTextInput";
 
 interface SignInScreenProps {
   screenHandler: (newScreen: ScreenType) => void;
 }
 
 const SignInScreen = ({ screenHandler }: SignInScreenProps) => {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const theme = useTheme();
 
-  const { signIn } = React.useContext(AuthContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayErrors, setDisplayErrors] = useState(false);
+  const [invalidCredentials, setInvalidCredentials] = useState(false);
+
+  const { signIn } = useContext(AuthContext);
+
+  const emailHandler = (newEmail: string) => {
+    setEmail(newEmail);
+  };
+
+  const emailErrorVisible = () => {
+    return !email.includes("@");
+  };
+
+  const emailErrorText = () => {
+    return "Invalid email address";
+  };
+
+  const passwordHandler = (newPassword: string) => {
+    setPassword(newPassword);
+  };
+
+  const passwordErrorVisible = () => {
+    return !(password.length >= 8 && password.length <= 32);
+  };
+
+  const passwordErrorText = () => {
+    return "Password should have between 8 and 32 characters";
+  };
 
   return (
     <SafeAreaScreenWrapper>
-      <View>
-        <Text>Please login</Text>
-        <TextInput label="email" value={email} onChangeText={(newEmail) => setEmail(newEmail)} />
-        <TextInput
-          label="password"
+      <View
+        style={{
+          justifyContent: "center",
+          height: "100%",
+          padding: 30,
+        }}>
+        <Text
+          variant="displaySmall"
+          style={{
+            textAlign: "center",
+            color: theme.colors.primary,
+            fontWeight: "bold",
+            marginBottom: 20,
+          }}>
+          Welcome back.
+        </Text>
+        <CustomTextInput
+          label="Email"
+          value={email}
+          valueHandler={emailHandler}
+          errorVisible={emailErrorVisible}
+          errorText={emailErrorText}
+          displayErrors={displayErrors}
+        />
+        <CustomTextInput
+          label="Password"
           value={password}
-          onChangeText={(newPassword) => setPassword(newPassword)}
+          valueHandler={passwordHandler}
+          errorVisible={passwordErrorVisible}
+          errorText={passwordErrorText}
+          displayErrors={displayErrors}
           secureTextEntry
         />
-        <Button onPress={() => signIn({ email, password })}>Login</Button>
-        <Button onPress={() => screenHandler("signUp")}>Go to sign up page</Button>
+        <Button
+          mode="contained"
+          style={{
+            borderRadius: 5,
+            marginTop: 20,
+          }}
+          onPress={async () => {
+            setDisplayErrors(true);
+            if (!emailErrorVisible() && !passwordErrorVisible()) {
+              const response = await signIn({ email, password });
+              if (!response.valueOf()) {
+                setInvalidCredentials(true);
+              }
+            }
+          }}>
+          LOGIN
+        </Button>
+        <HelperText type="error" visible={invalidCredentials}>
+          Invalid email or password.
+        </HelperText>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            padding: 10,
+          }}>
+          <Text>Don't have an account? </Text>
+          <Pressable onPress={() => screenHandler("signUp")}>
+            <Text style={{ color: theme.colors.primary, fontWeight: "bold" }}>Sign up</Text>
+          </Pressable>
+        </View>
       </View>
     </SafeAreaScreenWrapper>
   );
