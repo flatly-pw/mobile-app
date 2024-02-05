0972b91b6a79185685291ef3421ed03029cae792
zeraye <zeraye@protonmail.com>
2024-02-05 01:47:39 +0100

    feat: add editable email

--- features/ProfileTab/ProfileScreen/EditableEmail/EditableEmail.tsx
+++ features/ProfileTab/ProfileScreen/EditableEmail/EditableEmail.tsx
@@ -1,15 +1,60 @@
+import * as SecureStore from "expo-secure-store";
 import { useContext, useState } from "react";
 import { View } from "react-native";
-import { Button, Surface, Text, TextInput } from "react-native-paper";
+import { Button, Surface, Text, TextInput, ActivityIndicator } from "react-native-paper";
 
+import AuthContext from "../../../../contexts/AuthContext";
 import SettingsContext from "../../../../contexts/SettingsContext";
 import translations from "../../../../preferences/translations";
 
 const EditableEmail = () => {
   const { settings, setSettings } = useContext(SettingsContext);
+  const { signOut } = useContext(AuthContext);
 
   const [isEditable, setIsEditable] = useState(false);
   const [email, setEmail] = useState(settings.email);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const editEmail = async () => {
+    setIsLoading(true);
+    let userToken: string | null;
+    try {
+      userToken = await SecureStore.getItemAsync("userToken");
+    } catch {
+      // Token was not found in the secure store. User is not authenticated.
+      userToken = null;
+    }
+
+    if (!userToken) {
+      return;
+    }
+
+    const response = await fetch(process.env.EXPO_PUBLIC_API_URL + "/user/email", {
+      method: "PUT",
+      headers: {
+        Authorization: "Bearer " + userToken,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ newMail: email }),
+    });
+
+    if (response.ok) {
+      setSettings({ ...settings, email });
+      setIsEditable(false);
+      signOut();
+    } else {
+      console.log(
+        "Problem with fetch in editEmail, status text:",
+        response.statusText,
+        ", status code:",
+        response.status
+      );
+    }
+
+    setIsLoading(false);
+  };
 
   return (
     <Surface
@@ -35,13 +80,14 @@ const EditableEmail = () => {
           </>
         )}
       </View>
-      {isEditable ? (
+      {isLoading ? (
+        <ActivityIndicator animating={isLoading} size="large" style={{ padding: 10 }} />
+      ) : isEditable ? (
         <View>
           <Button
             icon="lead-pencil"
             onPress={() => {
-              setSettings({ ...settings, email });
-              setIsEditable(false);
+              editEmail();
             }}>
             {translations.APPLY[settings.language]}
           </Button>
