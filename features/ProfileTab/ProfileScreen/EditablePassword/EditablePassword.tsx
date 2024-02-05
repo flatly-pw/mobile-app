8e36f3ea4a8a090be1dea745ba4f9b42dd3bc8ab
zeraye <zeraye@protonmail.com>
2024-02-05 01:47:42 +0100

    feat: add editable password

--- features/ProfileTab/ProfileScreen/EditablePassword/EditablePassword.tsx
+++ features/ProfileTab/ProfileScreen/EditablePassword/EditablePassword.tsx
@@ -1,16 +1,63 @@
+import * as SecureStore from "expo-secure-store";
 import { useContext, useState } from "react";
 import { View } from "react-native";
-import { Button, Surface, Text, TextInput } from "react-native-paper";
+import { ActivityIndicator, Button, Surface, Text, TextInput } from "react-native-paper";
 
+import AuthContext from "../../../../contexts/AuthContext";
 import SettingsContext from "../../../../contexts/SettingsContext";
 import translations from "../../../../preferences/translations";
 
 const EditablePassword = () => {
   const { settings } = useContext(SettingsContext);
+  const { signOut } = useContext(AuthContext);
 
   const [isEditable, setIsEditable] = useState(false);
   const [oldPassword, setOldPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const editPassword = async () => {
+    setIsLoading(true);
+    let userToken: string | null;
+    try {
+      userToken = await SecureStore.getItemAsync("userToken");
+    } catch {
+      // Token was not found in the secure store. User is not authenticated.
+      userToken = null;
+    }
+
+    if (!userToken) {
+      return;
+    }
+
+    const response = await fetch(process.env.EXPO_PUBLIC_API_URL + "/user/password", {
+      method: "PUT",
+      headers: {
+        Authorization: "Bearer " + userToken,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        currentPassword: oldPassword,
+        newPassword,
+      }),
+    });
+
+    if (response.ok) {
+      setIsEditable(false);
+      signOut();
+    } else {
+      console.log(
+        "Problem with fetch in editPassword, status text:",
+        response.statusText,
+        ", status code:",
+        response.status
+      );
+    }
+
+    setIsLoading(false);
+  };
 
   return (
     <Surface
@@ -23,7 +70,9 @@ const EditablePassword = () => {
         marginTop: 20,
       }}>
       <View style={{ padding: 10, flex: 1 }}>
-        {isEditable ? (
+        {isLoading ? (
+          <ActivityIndicator animating={isLoading} size="large" style={{ padding: 10 }} />
+        ) : isEditable ? (
           <>
             <TextInput
               label={translations.OLD_PASSWORD[settings.language]}
@@ -50,7 +99,7 @@ const EditablePassword = () => {
           <Button
             icon="lead-pencil"
             onPress={() => {
-              setIsEditable(false);
+              editPassword();
             }}>
             {translations.APPLY[settings.language]}
           </Button>
